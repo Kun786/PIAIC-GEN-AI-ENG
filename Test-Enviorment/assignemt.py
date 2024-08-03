82c8fd0054c17362cad818ee3a19adb5cf20efd1
Kun786 <faziislive@gmail.com>
2024-08-04 04:50:23 +0500

    4 Aug 2024 (Lecture1, 2 PIAIC)

---
+++ Test-Enviorment/assignemt.py
@@ -0,0 +1,36 @@
+
+print ("\nA Solution:hello world")
+print ("\nB Solution:Muhammad Abdul Rehman")
+
+
+print ("\nA Solution:Muhammad Abdul Rehman")
+print ("\nB 1,2,3")
+
+
+print ("\nA Solution:Hello\nWorld")
+print ("\nB Solution:Hello\tWorld")
+
+
+print ("\nA Solution:apple, banana, cherry")
+print ("\nB Solution:1-2-3")
+
+
+print ("\nA Solution:Hello World")
+print ("\nB Solution:12")
+
+
+print ("\nA Solution:He said, \"Hello!\"")
+print ("\nB Solution:This is a backslash:\\.")
+
+
+print ("\nA Solution:I am " + str(20) + " years old")
+print ("\nB Solution:The number is " + str(5))
+
+
+print ("\nA Solution:")
+for i in range(1,6):
+    print(i)
+
+print ("\nA Solution:")
+for i in range(5,0,-1):
+    print(i)
